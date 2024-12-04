1f7df82daaed32d921a7a94062189f21802039b8
Zenesh Shrestha <shresthazenesh057@gmail.com>
2024-12-04 07:02:08 +0545

    Jaazmin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'users',
     "djoser",
     "rest_framework_simplejwt",
+    
 ]
 
 MIDDLEWARE = [
@@ -143,6 +145,48 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+JAZZMIN_SETTINGS = {
+    "site_title": "PetParichaye",
+    "welcome_sign": "Welcome to PetParichaye Login Page",
+    "show_ui_builder": True,
+    "site_brand": "PetParichaye",
+    "site_header": "PetParichaye",
+    "copyright": "PetParichaye",
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": True,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "sandstone",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": (
